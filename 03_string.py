3a17d5d0e78bc5e2493e6ccc6f8e3fe29db15793
Rishikesh11kr <rishikesh01119@gmail.com>
2024-04-26 21:18:12 +0530

    python program

--- 03_string.py
+++ 03_string.py
@@ -1,2 +1,12 @@
 # String Manipulaton
 print("hello"+ " " + "rishi" +"how are you")
+
+# Concatinaton 
+str1 = "hello"
+num = 10
+print= str1 + num
+
+str1 = "hello"
+num = 10
+print(str1 + str(num))
+# Using format method 
---
+++ 05_variables.py
@@ -0,0 +1,21 @@
+# Veriable : is like a container in which we storer our data.
+# values can vary , we can change the values of variable these are not fixed
+''' such as name , rollNo.....etc are variables 
+name= "rishi"
+rollNO= 11
+Sum= 6+5
+'''
+name="rishi"
+print ('hello',name)
+
+name ="aniket"
+print ('helllo',name)
+
+# some other ways to declear and initialize var
+name="rishi"
+name = "rishi"
+name2 = "aniket"
+print(len(name))  # This will print the length of the string "rishi"
+print(type(name))  # This will print the type of the variable name, which is 'str'
+print(type(name2))  # This will print the type of the variable name2, which is also 'str'
+print('hello', name)  # This will print 'hello rishi'
